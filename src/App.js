04c613b1ba5c5efac43d9c0492545c70fb299524
tanianasrin1 <tasrineva169@gmail.com>
2022-09-27 22:09:14 +0600

    navber page fixed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import AddBook from './Components/AddBook/AddBook';
 import Home from './Components/Home/Home';
 import NotFound from './Components/NotFound/NotFound';
+import Navber from './Components/Shared/Navber';
 import ShowBook from './Components/ShowBook/ShowBook';
 
 function App() {
   return (
     <div>
+      <Navber></Navber>
          <Routes>
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/home' element={<Home></Home>}></Route>
--- src/Components/Shared/Navber.js
+++ src/Components/Shared/Navber.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Navber = () => {
     return (
         <nav>
-            <Link to="/" className='nav-link'>
-                Home
-            </Link>
+            <Link to="/" className='nav-link'>Home</Link>
+            <Link to="/showBook" className='nav-link'>Show Book</Link>
+            <Link to="/addBook" className='nav-link'>Add Book</Link>
         </nav>
     );
 };
