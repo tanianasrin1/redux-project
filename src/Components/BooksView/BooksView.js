e8b8328aaee9e33ecabbd0e37aaaf305d0babba4
tanianasrin1 <tasrineva169@gmail.com>
2022-09-30 17:32:10 +0600

    table pages added

--- src/Components/BooksView/BooksView.js
+++ src/Components/BooksView/BooksView.js
@@ -7,6 +7,34 @@ const BooksView = () => {
     return (
         <div>
             <h2>List of Book</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books && books.map((book)=> {
+                        const {id, title, author} = book;
+                        console.log(book)
+                        return (
+                            <tr key={id}>
+                            <td> {id} </td>
+                            <td> {title} </td>
+                            <td> {author} </td>
+                            <td>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                            
+                        </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 };
