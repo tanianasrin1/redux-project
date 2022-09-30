ef56aa49fdb17813fe479bb6396a15d6fa6f2b7b
tanianasrin1 <tasrineva169@gmail.com>
2022-09-30 17:23:36 +0600

    store pages added

--- src/Components/BooksView/BooksView.js
+++ src/Components/BooksView/BooksView.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const BooksView = () => {
+    const books = useSelector((state) => state.booksReducer.books);
+    console.log(books);
     return (
         <div>
             <h2>List of Book</h2>
