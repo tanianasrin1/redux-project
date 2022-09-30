266cfbbd8fde2db84edb40d543d1b7266e231eff
tanianasrin1 <tasrineva169@gmail.com>
2022-09-30 17:43:58 +0600

    table pages added

--- src/Components/AddBook/AddBook.js
+++ src/Components/AddBook/AddBook.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddBook = () => {
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
     return (
         <div>
-            This is a add book page
+           <h2>Add Book</h2>
+           <form>
+                <div className='form-field'>
+                    <label htmlFor="title">Title: </label>
+                    <input type="text" id='title' name="title" value={title} onChange={
+                        (e)=> setTitle(e.target.value)} 
+                        required
+                        />
+
+                </div>
+                <div className='form-field'>
+                    <label htmlFor="author">Author: </label>
+                    <input type="text" id='author' name="author" value={author} onChange={
+                        (e)=> setAuthor(e.target.value)} 
+                        required
+                        />
+
+                </div>
+           </form>
         </div>
     );
 };
